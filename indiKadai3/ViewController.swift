73e43d830f4e2bc0e2755fe0e288adce429aa16e
Taishi Kusunose <taishi.kusunose@gmail.com>
2021-11-25 08:34:59 +0900

    Initial Commit

---
+++ indiKadai3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  indiKadai3
+//
+//  Created by Taishi Kusunose on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
